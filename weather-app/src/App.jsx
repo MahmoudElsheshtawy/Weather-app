845479618a28cc77d70cf052e49db37b17d9057a
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-02-07 09:31:15 -1200

    kk

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Search from './components/search/Search'
-
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 function App() {
 
 
   return (
     <div className='container'>
      <Search/>
+     <UilReact/>
     </div>
   )
 }
